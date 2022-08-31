8f3dd4c00a2850e6d5dfb5b928727a048813e569
Sams-AD <samueldiogo032@gmail.com>
2022-08-31 12:11:38 +0100

    Terceiro commit

---
+++ JAVASCRIPT - FUNDAMENTOS/Exercicios-js/Fundamentos/template.js
@@ -0,0 +1,7 @@
+const nome = 'Samuel'
+const string = 'Olá '
+
+//console.log(string + nome + '!')
+
+const template = `Olá ${nome}!`
+console.log(template)
--- JAVASCRIPT - FUNDAMENTOS/inicio.js
+++ JAVASCRIPT - FUNDAMENTOS/inicio.js
@@ -1,1 +1,5 @@
 let dados = (3, 0, 5, 6, 2, 7)
+
+for (let i = 0; i < dados.length; i++) {
+  console.log(dados.charAt(i))
+}
